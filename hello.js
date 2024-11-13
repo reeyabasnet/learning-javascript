65c977fd9bf3cd7b5f8460cec16ad3ae088a31e3
reeyabasnet <reeyabasnet555@gmail.com>
2024-11-13 02:11:06 -0800

    first commit

---
+++ hello.js
@@ -0,0 +1,28 @@
+console.log("i am reeya basnet")
+console.log(2+3)
+let bucket = "water"
+let iamNepali = true
+let age = 22
+console.log(bucket)
+
+let shoes= ["Adidas","caliber","goldstar"]
+console.log(shoes[0],shoes[1],shoes[2]) 
+let myinformation = {
+    name:"reeya",
+    age:22,
+    address:"tarahara"
+
+}
+console.log(myinformation.name,myinformation.age,myinformation.address)
+let data= {
+    dogs:["cat","dog","lion"]
+}
+console.log(data)
+console.log(data.dogs[0])
+let haha = [{name:"reeya"},{name:"ram"}]
+console.log(haha[0])
+console.log(haha[0])
+console.log(haha[0].name)
+
+
+
